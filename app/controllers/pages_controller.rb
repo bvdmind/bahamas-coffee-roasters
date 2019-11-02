30c99d47ae812d4bd770619c6352021f95851869
Duval Cash <duvalwcash@gmail.com>
2019-11-01 23:52:52 -0400

    Added Bulma via Yarn, created navbar, and hero scaffold for landing page.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def landing
+  end
+
+  def about_us
+  end
+
+  def about_coffee
+  end
+
+  def about_bcr
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#landing'
+  get 'pages/about_us'
+  get 'pages/about_coffee'
+  get 'pages/about_bcr'
+  get 'pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
