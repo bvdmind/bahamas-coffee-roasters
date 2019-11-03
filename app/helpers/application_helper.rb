b3b809a3f84c927bb45ba6db2820822e208c2d65
Duval Cash <duvalwcash@gmail.com>
2019-11-03 12:30:23 -0500

    Red, Green, Refactor. inserted dummy text and placeholder images for inital layout, and refined contact form/ footer.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,14 @@ module ApplicationHelper
 
   def flash_class(level)
     case level
-      when :notice then "is-success"
-      when :success then "is-success"
-      when :error then "is-error"
-      when :alert then "is-error"
+    when "notice" then "is-notice"
+    when "success" then "is-success"
+    when "error" then "is-error"
+    when "alert" then "is-error"
     end
+  end
+
+  def copyright_helper
+    "\u00A9 Copyright Bahamas Coffee Roasters " + Time.now.year.to_s + ". All rights reserved."
   end
 end
